81e2c4f8cbbb614e7b4190270153cb6b1e34ddc6
Berel Levy <berellevy@gmail.com>
2020-05-17 18:48:00 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,7 @@
 current_time = Time.now
 
-if Time.now.to_s % 2 == 0
+if current_time.to_i % 2 == 0
     puts "Even!"
-elsif Time.now.to_se % 2 != 0
+elsif current_time.to_i % 2 != 0
     puts "Odd!"
 end
